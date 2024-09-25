c0ca6ec4c41ca1b6958c248d69bb0bd9bb20382a
Bryan Nelson <BNelson986@github.com>
2024-09-25 13:37:17 -0700

    Driver file to provide results of all required programs from the assignment.

---
+++ submission.py
@@ -0,0 +1,9 @@
+import constraints
+import mars_planner
+import routefinder
+
+# driver function
+if __name__=="__main__" :
+    mars_planner.print_solutions()
+    routefinder.print_solutions()
+    constraints.print_solution()
